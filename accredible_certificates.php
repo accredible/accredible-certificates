334419af9de7f72897dbefc5d829188fe89fe6a4
Martin Ndibui <muhingan@gmail.com>
2025-05-09 17:24:26 +0300

    version bump

--- accredible_certificates.php
+++ accredible_certificates.php
@@ -7,7 +7,7 @@
  * Plugin Name: Accredible Certificates & Open Badges
  * Plugin URI: https://github.com/accredible/accredible-certificates
  * Description: Certificates, open badges and blockchain credentials. Create, update and manage them on your WordPress site.
- * Version: 1.4.9
+ * Version: 1.4.10
  * Author: Accredible
  * Author URI: https://www.accredible.com
  * License: GPLv2 or later
