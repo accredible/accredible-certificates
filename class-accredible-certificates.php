7e26b0cb29e8bb8d0f1a5bde95f00aeb48816d59
Martin Ndibui <muhingan@gmail.com>
2025-05-19 12:35:55 +0300

    Release/1.5.0 (#31)
    * DGR-1812 - Wordpress plugin updates (#30)
    
    * Add docker-compose files to project
    
    * Fix - settings file
    
    * Ignore ide, zip files
    
    * cleanup
    
    * Ensure register_setting has sanitization
    
    * Resolve widget warnings + errors
    
    * updated readme
    
    * widget updates + abspath checks
    
    * removed extract() usage
    
    * fix warnings + errors, added global constants
    
    * added abspath to settings file
    
    * resolve security errors + warnings for users_list
    
    * added nonce_verification
    
    * error handling updates
    
    * cleanup
    
    * add nonce field to admin page
    
    * updated invalid api-key settings warning + styling
    
    * cleanup
    
    * replace direct db call to fetch user count
    
    * cleanup
    
    * moved class definition checks to respective classes
    
    * fid db query errors + warnings
    
    * fix errors + warnings for post_type_template
    
    * add class definition check for accredible_widget
    
    * moved function declarations into class for accredible_widget
    
    * cleanup
    
    * removed global $post update calls
    
    * cleanup
    
    * updated readme with version bump
    
    * Added svnignore file
    
    * version bump
    
    * readme update
    
    * readme update
    
    * cleanup
    
    * fix sorting users list
    
    * cleanup
    
    * replace wpdb query with WP_User_Query
    
    * removed direct db calls for comments + posts
    
    * removed unused $wpdb
    
    * updated dockerfile with PHP CodeSniffer
    
    * prevent plugin auto install + updated readme
    
    * removed Academy Theme support
    
    * escape group options correctly
    
    * renamed files: users_list.php, settings.php, accredible_widget.php, and main plugin file
    
    * cleanup
    
    * release 1.5.0

---
+++ class-accredible-certificates.php
@@ -0,0 +1,229 @@
+<?php
+/**
+ * Accredible Certificates & Open Badges
+ *
+ * @package Accredible_Certificates
+ *
+ * Plugin Name: Accredible Certificates & Open Badges
+ * Plugin URI: https://github.com/accredible/accredible-certificates
+ * Description: Certificates, open badges and blockchain credentials. Create, update and manage them on your WordPress site.
+ * Version: 1.5.0
+ * Author: Accredible
+ * Author URI: https://www.accredible.com
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+defined( 'ABSPATH' ) || die;
+
+define( 'ACCREDIBLE_CERTIFICATES_VERSION', '1.5.0' );
+define( 'ACCREDIBLE_CERTIFICATES_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+define( 'ACCREDIBLE_CERTIFICATES_SCRIPT_VERSION_TOKEN', ACCREDIBLE_CERTIFICATES_VERSION );
+
+if ( ! defined( 'ACCREDIBLE_CERTIFICATES_PLUGIN_URL' ) ) {
+	$accredible_certificates_plugin_url = trailingslashit( WP_PLUGIN_URL . '/' . basename( __DIR__ ) );
+	$accredible_certificates_plugin_url = str_replace( array( 'https://', 'http://' ), array( '//', '//' ), $accredible_certificates_plugin_url );
+
+	/**
+	 * Define Accredible - Set the plugin relative URL.
+	 *
+	 * Will be set based on the WordPress define `WP_PLUGIN_URL`.
+	 *
+	 * @uses WP_PLUGIN_URL
+	 *
+	 * @var string $accredible_certificates_plugin_url URL to plugin install directory.
+	 */
+	define( 'ACCREDIBLE_CERTIFICATES_PLUGIN_URL', $accredible_certificates_plugin_url );
+}
+
+// For composer dependencies.
+require plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
+
+use ACMS\Api;
+
+require_once ACCREDIBLE_CERTIFICATES_PLUGIN_PATH . 'class-accredible-widget.php'; // Require Widget for credential display.
+require_once ACCREDIBLE_CERTIFICATES_PLUGIN_PATH . 'class-accredible-certificates-settings.php'; // Require Settings.
+
+if ( ! class_exists( 'Accredible_Certificates' ) ) {
+	/**
+	 * Accredible Certificate class
+	 */
+	class Accredible_Certificates {
+		/**
+		 * Accredible DB version
+		 *
+		 * @var string
+		 */
+		public static $accredible_db_version = '1.0.0';
+
+		/**
+		 * Construct the plugin object
+		 */
+		public function __construct() {
+			// Initialize Settings.
+			new Accredible_Certificates_Settings();
+
+			$plugin = plugin_basename( __FILE__ );
+			add_filter( "plugin_action_links_$plugin", array( $this, 'plugin_settings_link' ) );
+
+			add_action( 'admin_menu', array( $this, 'register_certificates_admin_menu_page' ) );
+
+			// Require accredible admin styles.
+			add_action( 'admin_enqueue_scripts', array( &$this, 'acc_load_plugin_css' ) );
+
+			add_action( 'hourly_certificate_issuance', array( &$this, 'sync_with_accredible' ) );
+
+			register_activation_hook( __FILE__, array( &$this, 'activate' ) );
+			register_deactivation_hook( __FILE__, array( &$this, 'deactivate' ) );
+		} // END public function __construct
+
+		/**
+		 * Activate the plugin
+		 */
+		public static function activate() {
+			// Update the DB.
+			self::accredible_db_install();
+
+			// Set auto issue to false by default.
+			add_option( 'automatically_issue_certificates', 0 );
+
+			// Cron job for automatic certificate creation.
+			wp_schedule_event( time(), 'hourly', 'hourly_certificate_issuance' );
+		} // END public static function activate
+
+		/**
+		 * Deactivate the plugin.
+		 */
+		public static function deactivate() {
+			// Remove job for automatic certificate creation.
+			wp_clear_scheduled_hook( 'hourly_certificate_issuance' );
+		} // END public static function deactivate
+
+
+		/**
+		 * Create the database table for course and group mapping
+		 */
+		public static function accredible_db_install() {
+			global $wpdb;
+
+			$table_name = $wpdb->prefix . 'accredible_mapping';
+
+			$charset_collate = $wpdb->get_charset_collate();
+
+			$sql = "CREATE TABLE $table_name (
+				id mediumint(9) NOT NULL AUTO_INCREMENT,
+				course_id mediumint(9) NOT NULL,
+				group_id mediumint(9) NOT NULL,
+				PRIMARY KEY  (id)
+			) $charset_collate;";
+
+			require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+			dbDelta( $sql );
+
+			add_option( 'accredible_db_version', self::$accredible_db_version );
+		}
+
+		/**
+		 * Add the settings link to the plugins page
+		 *
+		 * @param array $links links.
+		 * @return array $links
+		 */
+		public function plugin_settings_link( $links ) {
+			$settings_link = '<a href="' . admin_url( 'options-general.php?page=accredible_certificates' ) . '">Settings</a>';
+			array_unshift( $links, $settings_link );
+			return $links;
+		}
+
+		/**
+		 * Get an array of credentials for a particular email address
+		 *
+		 * @param string $email email.
+		 * @return \ACMS\stdObject $credentials
+		 */
+		public static function get_credentials_for_email( $email ) {
+			$api = new Api( get_option( 'api_key' ) );
+
+			$credentials = $api->get_credentials( null, $email );
+
+			return $credentials;
+		}
+
+		/**
+		 * Create a credential
+		 *
+		 * @param string $name name.
+		 * @param string $email email.
+		 * @param int    $group_id group id.
+		 * @return \ACMS\stdObject $response
+		 */
+		public static function create_credential( $name, $email, $group_id ) {
+			$api = new Api( get_option( 'api_key' ) );
+
+			return $api->create_credential( $name, $email, $group_id );
+		}
+
+		/**
+		 * Get all credential groups
+		 *
+		 * @return array $groups
+		 */
+		public static function get_groups() {
+			$api = new Api( get_option( 'api_key' ) );
+
+			$response = $api->get_groups( 1000, 1 );
+
+			return $response->groups;
+		}
+		/**
+		 * Register the admin menu item
+		 */
+		public function register_certificates_admin_menu_page() {
+			add_menu_page( 'Certificates & Badges', 'Certificates & Badges', 'list_users', 'certificates-admin', array( $this, 'render_certificates_admin_page' ), 'dashicons-tablet', 40 );
+		}
+
+		/**
+		 * Render the certificates admin page
+		 */
+		public function render_certificates_admin_page() {
+			require_once ACCREDIBLE_CERTIFICATES_PLUGIN_PATH . 'certificates-admin.php';
+		}
+
+		/**
+		 * Load the admin styles
+		 */
+		public static function acc_load_plugin_css() {
+			$version = defined( 'WP_DEBUG' ) && WP_DEBUG ? time() : ACCREDIBLE_CERTIFICATES_SCRIPT_VERSION_TOKEN;
+			wp_register_style( 'accredible-admin-style', plugins_url( '/css/style.css', __FILE__ ), array(), $version );
+			wp_enqueue_style( 'accredible-admin-style' );
+		}
+
+		/**
+		 * Send batch requests via the ACMS API
+		 *
+		 * @param Array $requests requests.
+		 * @return mixed $response
+		 */
+		public static function batch_requests( $requests ) {
+			$api = new Api( get_option( 'api_key' ) );
+
+			$request_count = count( $requests );
+			for ( $i = 0; $i < $request_count; $i++ ) {
+				$requests[ $i ]['url'] = 'v1/' . $requests[ $i ]['url'];
+			}
+
+			$response = $api->send_batch_requests( $requests );
+
+			return $response;
+		}
+	} // END class Accredible_Certificates.
+}
+
+if ( class_exists( 'Accredible_Certificates' ) ) {
+	// Installation and uninstallation hooks.
+	register_activation_hook( __FILE__, array( 'Accredible_Certificates', 'activate' ) );
+	register_deactivation_hook( __FILE__, array( 'Accredible_Certificates', 'deactivate' ) );
+
+	// Instantiate the plugin class.
+	$accredible_certificate = new Accredible_Certificates();
+}
---
+++ class-users-list.php
@@ -0,0 +1,505 @@
+<?php
+/**
+ * Users List Table for Accredible Certificates
+ *
+ * @package Accredible_Certificates
+ */
+
+defined( 'ABSPATH' ) || die;
+
+if ( ! class_exists( 'WP_List_Table' ) ) {
+	// Require the list table class.
+	require_once ABSPATH . 'wp-admin/includes/class-wp-list-table.php';
+}
+
+if ( ! class_exists('Accredible_Certificates') ) {
+	require_once ACCREDIBLE_CERTIFICATES_PLUGIN_PATH . 'class-accredible-certificates.php';
+}
+
+/**
+ * Class Users_List
+ *
+ * Handles the display and management of users list table for Accredible certificates.
+ * Extends WP_List_Table to create a custom table for managing certificate recipients.
+ *
+ * @package    Accredible_Certificates
+ */
+class Users_List extends WP_List_Table {
+	/**
+	 * Flag to indicate if there are no groups available.
+	 *
+	 * @var boolean
+	 */
+	public $no_groups = false;
+
+	/** Class constructor */
+	public function __construct() {
+
+		parent::__construct(
+			array(
+				'singular' => __( 'Recipient', 'accredible-certificates' ), // Singular name of the listed records.
+				'plural'   => __( 'Recipients', 'accredible-certificates' ), // Plural name of the listed records.
+				'ajax'     => false, // Does this table support ajax?
+			)
+		);
+	}
+
+	/**
+	 * Retrieve users' data from the database.
+	 *
+	 * @param int $per_page    Number of users to retrieve per page.
+	 * @param int $page_number Current page number.
+	 *
+	 * @return mixed
+	 */
+	public static function get_users( $per_page = 20, $page_number = 1 ) {
+		$accredible_certificates = new Accredible_Certificates();
+
+		// Define allowed columns for ordering.
+		$allowed_columns = array(
+			'id',
+			'user_login',
+			'user_nicename',
+			'user_email',
+		);
+
+		// Get and validate orderby parameter.
+		$order_by = self::sanitize_request_parameter( 'orderby' );
+		if ( ! in_array( $order_by, $allowed_columns, true ) ) {
+			$order_by = 'id';
+		}
+
+		// Get and validate order parameter.
+		$order = self::sanitize_request_parameter( 'order' );
+		$order = strtoupper( $order );
+		if ( ! in_array( $order, array( 'ASC', 'DESC' ), true ) ) {
+			$order = 'DESC';
+		}
+
+		$search_term = self::sanitize_request_parameter( 's' );
+
+		// Prepare query arguments.
+		$args = array(
+			'number'  => $per_page,
+			'offset'  => ( $page_number - 1 ) * $per_page,
+			'orderby' => $order_by,
+			'order'   => $order,
+			'fields'  => array( 'ID', 'user_login', 'user_nicename', 'user_email' ),
+		);
+
+		// Add search if provided.
+		if ( ! empty( $search_term ) ) {
+			$args['search']         = '*' . $search_term . '*';
+			$args['search_columns'] = array( 'user_login', 'user_email' );
+		}
+
+		// Create the query.
+		$user_query = new WP_User_Query( $args );
+		$users      = $user_query->get_results();
+
+		// Format results to match the previous structure.
+		$result = array();
+		foreach ( $users as $user ) {
+			$result[] = array(
+				'id'            => $user->ID,
+				'user_login'    => $user->user_login,
+				'user_nicename' => $user->user_nicename,
+				'user_email'    => $user->user_email,
+			);
+		}
+
+		// Don't attempt this query if there are no users.
+		$result_count = count( $result );
+		if ( $result_count > 0 ) {
+			// Batch request to get user credentials.
+			$requests = array();
+			for ( $x = 0; $x < $result_count; $x++ ) {
+				$requests[] = array(
+					'method' => 'get',
+					'url'    => 'all_credentials',
+					'params' => array( 'email' => strtolower( $result[ $x ]['user_email'] ) ),
+				);
+			}
+
+			try {
+				$response = Accredible_Certificates::batch_requests( $requests );
+			} catch ( Exception $e ) {
+				// Create a WP_Error object for proper error handling.
+				$error = new WP_Error(
+					'accredible_certificate_error',
+					$e->getMessage(),
+					array(
+						'requests' => $requests,
+						'status'   => 'error',
+					)
+				);
+
+				// Display user-friendly error message.
+				echo '<div class="notice notice-error is-dismissible">';
+				echo '<p>' . esc_html__( 'Error fetching credentials. Please try again later.', 'accredible-certificates' ) . '</p>';
+				echo '</div>';
+
+				// If WP_DEBUG is enabled, log the error.
+				if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
+					// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+					error_log( $error->get_error_message() );
+				}
+			}
+
+			$response_results_count = count( $response->results );
+			for ( $i = 0; $i < $response_results_count; $i++ ) {
+				if ( 'Not Found' !== $response->results[ $i ]->body ) {
+					$credentials                 = json_decode( $response->results[ $i ]->body );
+					$result[ $i ]['credentials'] = $credentials->credentials;
+				}
+			}
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Returns the count of users in the database.
+	 *
+	 * @return null|string
+	 */
+	public static function record_count() {
+		$user_count = count_users();
+		return $user_count['total_users'];
+	}
+
+
+	/** Text displayed when no user data is available */
+	public function no_items() {
+		esc_html_e( 'No users available.', 'accredible-certificates' );
+	}
+
+	/**
+	 * Render a column when no column-specific method exists.
+	 *
+	 * @param array  $item item.
+	 * @param string $column_name column name.
+	 *
+	 * @return mixed
+	 */
+	public function column_default( $item, $column_name ) {
+		switch ( $column_name ) {
+			case 'user_login':
+			case 'user_nicename':
+			case 'user_email':
+				return $item[ $column_name ];
+			case 'credentials':
+				return $this->column_credentials( $item );
+			default:
+				// Return a formatted display of the column value if it exists.
+				if ( isset( $item[ $column_name ] ) ) {
+					return esc_html( $item[ $column_name ] );
+				}
+				// Return a dash for empty or undefined columns.
+				return '—';
+		}
+	}
+
+	/**
+	 * Render the bulk edit checkbox
+	 *
+	 * @param array $item item.
+	 *
+	 * @return string
+	 */
+	public function column_cb( $item ) {
+		return sprintf(
+			'<input type="checkbox" name="credential_users[]" value="%s" />',
+			$item['id']
+		);
+	}
+
+	/**
+	 * Render items in the credential column
+	 *
+	 * @param array $item item.
+	 * @return string
+	 */
+	public function column_credentials( $item ) {
+		$string = '';
+		foreach ( $item['credentials'] as $credential ) {
+			$string = $string . "<a href='" . $credential->url . "' target='_blank'>" . $credential->url . '</a><br>';
+		}
+		return $string;
+	}
+
+	/**
+	 *  Associative array of columns
+	 *
+	 * @return array
+	 */
+	public function get_columns() {
+		$columns = array(
+			'cb'            => '<input type="checkbox" />',
+			'user_login'    => 'Login',
+			'user_nicename' => 'Username',
+			'user_email'    => 'Email',
+			'credentials'   => 'Credentials',
+		);
+
+		return $columns;
+	}
+
+	/**
+	 * Columns to make sortable.
+	 *
+	 * @return array
+	 */
+	public function get_sortable_columns() {
+		$sortable_columns = array(
+			'user_login'    => array( 'user_login', true ),
+			'user_nicename' => array( 'user_nicename', false ),
+			'user_email'    => array( 'user_email', false ),
+		);
+
+		return $sortable_columns;
+	}
+
+	/**
+	 * Get the select options for the group dropdown.
+	 *
+	 * @return string
+	 */
+	public function get_group_select_options() {
+		$accredible_certificates = new Accredible_Certificates();
+		try {
+			$groups = Accredible_Certificates::get_groups();
+		} catch ( Exception $e ) {
+			$groups = array();
+			$this->display_admin_notice( 'Error fetching groups. Please try again later.', 'error' );
+		}
+
+		$options      = '';
+		$groups_count = count( $groups );
+		for ( $i = 0; $i < $groups_count; $i++ ) {
+			$options .= "\n\t<option value='" . esc_attr( $groups[ $i ]->id ) . "'>" . esc_html( $groups[ $i ]->name ) . '</option>';
+		}
+
+		// set the flag to show there are no groups.
+		if ( 0 === $groups_count ) {
+			$this->no_groups = true;
+		}
+
+		return $options;
+	}
+
+	/**
+	 * Method to override the header nav and add our groups dropdown and button - https://github.com/WordPress/WordPress/blob/eeefec932f3d4f3b50369f6523c2cd8fad3d467f/wp-admin/includes/class-wp-users-list-table.php#L259
+	 *
+	 * @param type $which which.
+	 */
+	public function extra_tablenav( $which ) {
+
+		$id = 'bottom' === $which ? 'group_id2' : 'group_id';
+		?>
+	<div class="alignleft actions">
+		<label class="screen-reader-text" for="<?php echo esc_attr( $id ); ?>"><?php esc_html_e( 'Select Group', 'accredible-certificates' ); ?></label>
+		<select name="<?php echo esc_attr( $id ); ?>" id="<?php echo esc_attr( $id ); ?>">
+			<option value=""><?php esc_html_e( 'Select Group', 'accredible-certificates' ); ?></option>
+			<?php
+			$allowed_tags = array(
+				'option' => array(),
+			);
+			echo wp_kses( $this->get_group_select_options(), $allowed_tags );
+			?>
+		</select>
+		<?php
+		submit_button( __( 'Create Credentials', 'accredible-certificates' ), '', 'create-credentials', false, 'onclick="setTimeout(disableCertificateSubmitButton, 1)"' );
+		echo '</div>';
+	}
+
+	/**
+	 * Handles data query and filter, sorting, and pagination.
+	 */
+	public function prepare_items() {
+
+		$columns               = $this->get_columns();
+		$hidden                = array();
+		$sortable              = $this->get_sortable_columns();
+		$this->_column_headers = array( $columns, $hidden, $sortable );
+
+		/** Process bulk action */
+		$this->process_bulk_action();
+
+		// Avoid bulk request overflows by limiting the page size.
+		$per_page     = 20;
+		$current_page = $this->get_pagenum();
+		$total_items  = self::record_count();
+
+		$this->set_pagination_args(
+			array(
+				'total_items' => $total_items, // WE have to calculate the total number of items.
+				'per_page'    => 20, // WE have to determine how many items to show on a page.
+			)
+		);
+
+		$this->items = self::get_users( 20, $current_page );
+	}
+
+	/**
+	 * Capture the bulk action required, and return it.
+	 *
+	 * Overridden from the base class implementation to capture
+	 * the role change drop-down.
+	 *
+	 * @since  3.1.0
+	 * @access public
+	 *
+	 * @return string The bulk action required.
+	 */
+	public function current_action() {
+		$create_credentials = self::sanitize_request_parameter( 'create-credentials' );
+		if ( ! $create_credentials ) {
+			return parent::current_action();
+		}
+
+		$group_id  = self::sanitize_request_parameter( 'group_id' );
+		$group_id2 = self::sanitize_request_parameter( 'group_id2' );
+
+		if ( ! empty( $group_id ) || ! empty( $group_id2 ) ) {
+			return 'create-credentials';
+		}
+
+		if ( empty( $group_id ) && empty( $group_id2 ) ) {
+			// Let the user know they need to select a group.
+			echo '<div class="notice notice-error is-dismissible">';
+			echo '<p>You need to select a Group to create Credentials.</p>';
+			echo '</div>';
+		}
+
+		return parent::current_action();
+	}
+
+	/**
+	 * When the action is submitted, we should do what the user suggested - make credentials
+	 */
+	public function process_bulk_action() {
+
+		// Detect when a bulk action is being triggered...
+		if ( 'create-credentials' === $this->current_action() ) {
+			$accredible_certificates = new Accredible_Certificates();
+
+			// Verify nonce.
+			$nonce = self::sanitize_post_parameter( 'accredible_certificates_nonce' );
+			self::verify_nonce( $nonce, 'accredible_certificates_bulk_action' );
+
+			// Get and validate group_id from either group_id or group_id2.
+			$group_id = self::sanitize_post_parameter( 'group_id' );
+			if ( empty( $group_id ) ) {
+				$group_id = self::sanitize_post_parameter( 'group_id2' );
+			}
+
+			// Get and validate credential_users array.
+			$users = self::get_credential_users();
+
+			// Only proceed if we have both a group_id and users.
+			if ( ! empty( $group_id ) && ! empty( $users ) ) {
+				// Create credentials for each user.
+				foreach ( $users as $user_id ) {
+					// Find the user.
+					$userdata = WP_User::get_data_by( 'id', $user_id );
+					if ( ! $userdata ) {
+						continue;
+					}
+
+					$user_firstname = get_user_meta( $user_id, 'first_name', true );
+					$user_lastname  = get_user_meta( $user_id, 'last_name', true );
+
+					$recipient_name = ( $user_firstname && $user_lastname )
+						? $user_firstname . ' ' . $user_lastname
+						: $userdata->display_name;
+
+					// Create a credential.
+					try {
+						$credential = Accredible_Certificates::create_credential(
+							$recipient_name,
+							$userdata->user_email,
+							$group_id
+						);
+					} catch ( Exception $e ) {
+						$this->display_admin_notice( 'Failed to create credential for ' . $userdata->user_email, 'error' );
+					}
+				}
+				$this->display_admin_notice( 'Credentials created!', 'success' );
+			} else {
+				$this->display_admin_notice( 'Failed to create credentials. Please ensure you have selected both a group and users.', 'error' );
+			}
+		}
+	}
+
+	/**
+	 * Process and sanitize the credential users array from POST data.
+	 *
+	 * @return array Array of sanitized user IDs.
+	 */
+	private static function get_credential_users() {
+		$users = array();
+		// nonce verification is handled in the process_bulk_action method.
+		// phpcs:disable WordPress.Security.NonceVerification, WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+		$credential_users = isset( $_POST['credential_users'] ) ? wp_unslash( $_POST['credential_users'] ) : array();
+		// phpcs:enable WordPress.Security.NonceVerification, WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+
+		if ( is_array( $credential_users ) ) {
+			$users = array_map( 'absint', array_keys( $credential_users ) );
+		}
+
+		return $users;
+	}
+
+	/**
+	 * Verify WP nonce for the action.
+	 *
+	 * @param string $nonce Nonce value that was used for verification.
+	 * @param string $action Should give context to what is taking place and be the same when nonce was created.
+	 */
+	private static function verify_nonce( $nonce, $action ) {
+		if ( ! ( isset( $nonce ) && wp_verify_nonce( $nonce, $action ) ) ) {
+			wp_die( 'Invalid nonce.' );
+		}
+	}
+
+	/**
+	 * Sanitize a string parameter.
+	 *
+	 * @param string      $key The key of the parameter.
+	 * @param string|null $default_value The default value if the parameter is not set.
+	 */
+	private static function sanitize_request_parameter( $key, $default_value = null ) {
+		// nonce verification is handled in the current_action method.
+		return isset( $_REQUEST[ $key ] ) ? sanitize_key( wp_unslash( $_REQUEST[ $key ] ) ) : $default_value; // phpcs:ignore WordPress.Security.NonceVerification
+	}
+
+	/**
+	 * Sanitize a POST parameter.
+	 *
+	 * @param string      $key The key of the parameter.
+	 * @param string|null $default_value The default value if the parameter is not set.
+	 */
+	private static function sanitize_post_parameter( $key, $default_value = null ) {
+		// nonce verification is handled in the process_bulk_action method.
+		return isset( $_POST[ $key ] ) ? sanitize_text_field( wp_unslash( $_POST[ $key ] ) ) : $default_value; // phpcs:ignore WordPress.Security.NonceVerification
+	}
+
+	/**
+	 * Display an admin notice.
+	 *
+	 * @param string $message The message to display.
+	 * @param string $type    The type of notice: 'success' or 'error'.
+	 */
+	private function display_admin_notice( $message, $type = 'success' ) {
+		$allowed_types = array( 'success', 'error' );
+		if ( ! in_array( $type, $allowed_types, true ) ) {
+			$type = 'success';
+		}
+		echo '<div class="notice notice-' . esc_attr( $type ) . ' is-dismissible">';
+		echo '<p>' . esc_html( $message ) . '</p>';
+		echo '</div>';
+	}
+}
+
+?>
