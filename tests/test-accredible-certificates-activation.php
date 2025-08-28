d925b720b1712a5e89454de5d32de8aec46f05d5
Martin Ndibui <muhingan@gmail.com>
2025-08-28 11:46:51 +0300

    DGR-1981 - Add WordPress plugin CI (#37)
    * Add CI
    
    * fix php_codesniffer installation
    
    * simplify ci
    
    * add missing dependencies
    
    * fix paths
    
    * small fix
    
    * set version constraints
    
    * add missing dependency
    
    * check
    
    * use composer cache, fix missing phpcsutils error
    
    * ignore html templates
    
    * let phpcs rely on the config .phpcs.xml.dist
    
    * update phpcs config + cleanup files
    
    * include phpcompatibility
    
    * add unit_tests job
    
    * update unit_tests job
    
    * include svn
    
    * disable sql ready step
    
    * syntax updates
    
    * grant wp user privileges
    
    * (temp) echo db searches
    
    * rename wp db
    
    * skip db creation
    
    * comment out unnecessary steps
    
    * re-add svn installation
    
    * revert - (temp) echo db searches
    
    * add php 5.6, composer 1, wp 6.1
    
    * failing test fix - update assertion
    
    * add name to ci job
    
    * assertion updates
    
    * upgrade min php version
    
    * use composer v2 for php7.4
    
    * add php 8.3, composer 2, wp 6.8
    
    * move composer step into PHP installation step
    
    * update php install step
    
    * add phpunit tool
    
    * update phpunit location
    
    * cleanup
    
    * remove caching + fix phpunit location
    
    * cleanup
    
    * cleanup
    
    * cleanup
    
    * remove push workflow trigger

--- tests/test-accredible-certificates-activation.php
+++ tests/test-accredible-certificates-activation.php
@@ -174,18 +174,18 @@ class Test_Accredible_Certificates_Activation extends WP_UnitTestCase {
 
 		// Test id column.
 		$this->assertArrayHasKey( 'id', $column_map );
-		$this->assertEquals( 'mediumint', $column_map['id']->Type );
+		$this->assertStringContainsString( 'mediumint', $column_map['id']->Type );
 		$this->assertEquals( 'NO', $column_map['id']->Null );
 		$this->assertEquals( 'auto_increment', $column_map['id']->Extra );
 
 		// Test course_id column.
 		$this->assertArrayHasKey( 'course_id', $column_map );
-		$this->assertEquals( 'mediumint', $column_map['course_id']->Type );
+		$this->assertStringContainsString( 'mediumint', $column_map['course_id']->Type );
 		$this->assertEquals( 'NO', $column_map['course_id']->Null );
 
 		// Test group_id column.
 		$this->assertArrayHasKey( 'group_id', $column_map );
-		$this->assertEquals( 'mediumint', $column_map['group_id']->Type );
+		$this->assertStringContainsString( 'mediumint', $column_map['group_id']->Type );
 		$this->assertEquals( 'NO', $column_map['group_id']->Null );
 	}
 
