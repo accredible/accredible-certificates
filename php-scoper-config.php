aa93b44e719311b955377a37a7ebb4a28b117a1c
Martin Ndibui <muhingan@gmail.com>
2025-12-10 12:00:49 +0300

    fix php-scoper config

--- php-scoper-config.php
+++ php-scoper-config.php
@@ -7,27 +7,25 @@
 
 declare(strict_types=1);
 
+use Isolated\Symfony\Component\Finder\Finder;
+
 return array(
 	// Prefix applied to all vendor code.
 	'prefix'                     => 'Accredible\Vendor',
 
 	// Only scope the vendor directory, exclude plugin files.
 	'finders'                    => array(
-		array(
-			'in'      => 'vendor',
-			'name'    => '*.php',
-			'exclude' => array(
-				'vendor/*/tests',
-				'vendor/*/Tests',
-				'vendor/*/test',
-				'vendor/*/Test',
-			),
-		),
+		Finder::create()
+			->files()
+			->in('vendor')
+			->name('*.php')
+			->exclude(array(
+				'tests',
+				'Tests',
+				'test',
+				'Test',
+			)),
 	),
-
-	// Whitelist global functions and classes that shouldn't be prefixed.
-	'whitelist-global-functions' => true,
-	'whitelist-global-classes'   => true,
 
 	// Exclude files from being scoped (plugin files should not be scoped).
 	'exclude-files'              => array(),
